f155e688d1bb55b7704e1740cf7ad5ccaaae1882
Gareth Allan <gareth.allan@gmail.com>
2021-12-01 23:59:19 +0000

    Day 1, Part 1

--- day1/index.test.ts
+++ day1/index.test.ts
@@ -1,9 +1,46 @@
-import generateMessage from './index'
+import { calculateNumberOfDepthIncreases } from './index'
 
-describe('generateMessage', () => {
-  it('Should return the expected message', () => {
-    const actualMessage = generateMessage()
+describe('calculateNumberOfDepthIncreases', () => {
+  it('should correctly calculate the number of times the depth increases', () => {
+    const actualNumberOfDepthIncreases = calculateNumberOfDepthIncreases([
+      '100',
+      '101',
+      '102'
+    ])
 
-    expect(actualMessage).toBe('Hello world!')
+    expect(actualNumberOfDepthIncreases).toBe(2)
+  })
+
+  it('should ignore depth decreases', () => {
+    const actualNumberOfDepthIncreases = calculateNumberOfDepthIncreases([
+      '199',
+      '200',
+      '208',
+      '210',
+      '200',
+      '207',
+      '240',
+      '269',
+      '260',
+      '263'
+    ])
+
+    expect(actualNumberOfDepthIncreases).toBe(7)
+  })
+
+  it('should return zero if an empty array is passed', () => {
+    const actualNumberOfDepthIncreases = calculateNumberOfDepthIncreases([])
+
+    expect(actualNumberOfDepthIncreases).toBe(0)
+  })
+
+  it('should raise an error if a non-numeric line is encountered', () => {
+    expect(() => {
+      calculateNumberOfDepthIncreases([
+        '100',
+        'Foo',
+        '102'
+      ])
+    }).toThrowError()
   })
 })
--- day1/index.ts
+++ day1/index.ts
@@ -1,5 +1,32 @@
-export default function generateMessage (): string {
-  return 'Hello world!'
+import { readFileSync } from 'fs'
+
+export function readInput(filename: string): string[] {
+  return readFileSync(filename, 'utf8').split('\n')
 }
 
-console.log(generateMessage())
+export function calculateNumberOfDepthIncreases(depthMeasurements: string[]): number {
+  let numberOfDepthIncreases = 0
+
+  depthMeasurements.forEach((line, currentIndex, lines) => {
+    if (currentIndex === 0) {
+      return
+    }
+
+    const currentDepth = parseInt(line)
+    const previousDepth = parseInt(lines[currentIndex - 1])
+
+    if (!currentDepth || !previousDepth) {
+      throw Error('Could not parse the input file!')
+    }
+
+    if (currentDepth > previousDepth) {
+      numberOfDepthIncreases++
+    }
+  })
+
+  return numberOfDepthIncreases
+}
+
+const input = readInput('day1/input.txt')
+const numberOfDepthIncreases = calculateNumberOfDepthIncreases(input)
+console.log('Number of depth increases: %d', numberOfDepthIncreases)
